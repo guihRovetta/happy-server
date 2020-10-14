93f518e039932edc0be344025b10e8f79eb8f10b
guihRovetta <guilherme020213@hotmail.com>
2020-10-13 21:39:28 -0300

    ✨ create first route to create orphanage

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,39 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+
 import './database/connection';
+import Orphanage from './models/Orphanage';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/users', (request, response) => {
-  return response.json({ message: 'Hiii!' });
+app.post('/orphanages', async(request, response) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  } = request.body;
+
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  });
+
+  await orphanagesRepository.save(orphanage);
+
+  return response.json({ message: "Hi" });
 });
 
 app.listen(3333);
